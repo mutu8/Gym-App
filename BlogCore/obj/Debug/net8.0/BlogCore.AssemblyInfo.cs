63faa4f05c158a76ed7f6be42e2bb90d53b7134e
Steven <yordy65@hotmail.com>
2024-12-12 05:15:31 -0500

    modification-footer

--- BlogCore/obj/Debug/net8.0/BlogCore.AssemblyInfo.cs
+++ BlogCore/obj/Debug/net8.0/BlogCore.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlogCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae4afb89060fa3bcd78debd299fdc5009d57e04e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67d11d062f283816199dc6a183fc800d62e4476f")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlogCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlogCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
